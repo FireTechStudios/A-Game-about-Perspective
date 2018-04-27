4d8f6ab3a12ae064ef7c090878c32b297998cc9d
FireTechStudios <laidlawawz@gmail.com>
2018-04-27 11:35:23 +0100

    Start Changes

---
+++ A Game about Perspective/Assets/Scripts/ChangeCamera.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+
+public class ChangeCamera : MonoBehaviour
+{
+    public GameObject camera2d;
+    public GameObject camera3d;
+    public bool isin3d = false;
+    public bool canbein3d = true;
+    public bool grantFullcontrols = false;
+    public float timeleftin3d = 10f;
+
+    //<-- START CAMERA CONTROLS -->//
+
+    void Start()
+    {
+        camera2d.SetActive(true);
+        camera3d.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (canbein3d == true && timeleftin3d <= 10)
+            {
+                camera3d.SetActive(true);
+                camera2d.SetActive(false);
+                canbein3d = false;
+                isin3d = true;
+            }
+            else if (canbein3d == false && timeleftin3d <= 10)
+            {
+                camera3d.SetActive(false);
+                camera2d.SetActive(true);
+                canbein3d = true;
+                isin3d = false;
+            }
+        }
+
+//<-- START PLAYER CONTROLS -->//
+
+        if (isin3d == true)
+        {
+            grantFullcontrols = true;
+        }
+
+        //<-- END PLAYER CONTROLS -->//
+
+    }
+
+    void FixedUpdate()
+    {
+        if (isin3d == true)
+        {
+            timeleftin3d -= Time.deltaTime;
+        }
+
+
+        if (timeleftin3d >= 10)
+        {
+            canbein3d = true;
+            timeleftin3d = 10;
+        }
+
+        if (timeleftin3d <= 0)
+        {
+            camera3d.SetActive(false);
+            camera2d.SetActive(true);
+            canbein3d = false;
+            isin3d = false;
+            grantFullcontrols = false;
+            timeleftin3d = 0;
+        }
+
+        if (timeleftin3d < 10)
+        {
+            if (isin3d == false)
+            {
+                timeleftin3d += Time.deltaTime;
+            }
+        }
+    }
+
+    //<-- END CAMERA CONTROLS -->//
+}
---
+++ A Game about Perspective/Assets/Scripts/CheckGrounded.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckGrounded : MonoBehaviour {
+
+    public bool IsGrounded = true;
+
+    void OnCollisionStay(Collision col)
+    {
+        IsGrounded = true;
+    }
+
+    void OnCollisionExit(Collision col)
+    {
+        if (col.gameObject.tag.Equals("Untagged"))
+        {
+            IsGrounded = true;
+        }
+        else
+        {
+            IsGrounded = false;
+        }
+
+    }
+
+}
