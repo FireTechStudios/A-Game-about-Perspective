c6907883eab6c2462e55e3dac210f337c3299412
FireTechStudios <laidlawawz@gmail.com>
2018-04-30 07:56:44 +0100

    First Build

---
+++ A Game about Perspective/Assets/Scripts/selectLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class selectLevel : MonoBehaviour {
+
+    [SerializeField]
+    public int level;
+
+    public Button levelButton;
+
+    // Use this for initialization
+    void Start () {
+        int getLevel = MenuController.selectedLevelInt;
+        Button btn = levelButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        MenuController.selectedLevelInt = level;
+    }
+}
