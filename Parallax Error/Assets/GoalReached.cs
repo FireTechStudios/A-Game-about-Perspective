18f7266de8203e6e97ea399fb7b22a07cd9feb88
FireTechStudios <laidlawawz@gmail.com>
2018-11-14 08:02:29 +0000

    Goal Screen work

---
+++ Parallax Error/Assets/GoalReached.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+public class GoalReached : MonoBehaviour {
+
+    public TextMeshProUGUI Title;
+    public TextMeshProUGUI Deaths;
+    public TextMeshProUGUI Time;
+    public string level;
+
+    // Use this for initialization
+    void Start () {
+        level = SceneManager.GetActiveScene().name;
+        Title.text = level+" Complete";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
